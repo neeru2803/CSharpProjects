55cd5c1360f102e3d2512cc3a542e44d9c8172a7
neeru2803 <neeru2803@siu.edu>
2015-04-28 23:57:12 -0500

    addition of binary numbers

---
+++ AddBinary/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddBinary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the value of a");
+            string a = Console.ReadLine();
+            int c = Convert.ToInt32(a, 2);
+            Console.WriteLine("enter the value of b");
+            string b = Console.ReadLine();
+            int d = Convert.ToInt32(b, 2);
+
+            int e = c + d;
+            string sol = Convert.ToString(e, 2);
+
+            Console.WriteLine("the given value is :{0}", sol);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Strings/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s, revs;
+            Console.WriteLine("enter the string s");
+            s = Console.ReadLine();
+            Console.WriteLine("enter the string revs");
+            revs = Console.ReadLine();
+            int length = s.Length;
+            
+            for (int i = 0;i<length;i++)
+            {
+                string[] temps = new string[i];
+            }
+            if (revs == s)
+            {
+                Console.WriteLine("the string is matched", s, revs);
+            }
+            else
+            {
+                Console.WriteLine("the string is not matched", s, revs);
+            }
+            Console.ReadKey();
+             
+
+
+
+
+
+
+
+        }
+    }
+}
