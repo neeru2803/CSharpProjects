a2683b922dcd9b319a716095f3c17d48280cb91b
neeru2803 <neeru2803@siu.edu>
2015-06-08 17:45:57 -0500

    REMVED N ERROR RELATED TO NAMESPACE

--- AutomobileApp/AutomobileApp/Models/Category.cs
+++ AutomobileApp/AutomobileApp/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AutomobileApp.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -18,6 +19,7 @@ namespace AutomobileApp.Models
         public string Description { get; set; }
 
         public virtual ICollection<Product> Products { get; set; }
+    
 
     }
 }
